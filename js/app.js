e1c3bceafd93a6c36405101e077c133bac383a84
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-05 18:25:15 +0600

    lkdj;f

--- js/app.js
+++ js/app.js
@@ -31,24 +31,17 @@ const displayPhones = (phones, dataLimit) => {
     phoneDiv.classList.add("col");
     phoneDiv.innerHTML = `
     <div class="card p-4">
-  <img src="${phone.image}" class="card-img-top" alt="..." />
-  <div class="card-body">
-    <h5 class="card-title">${phone.phone_name}</h5>
-    <p class="card-text">
-      This is a longer card with supporting text below as a natural lead-in to
-      additional content. This content is a little bit longer.
-    </p>
-    <button
-      onclick="loadPhoneDetails('${phone.slug}')"
-      href="#"
-      class="btn btn-primary" data-bs-toggle="modal"
-      data-bs-target="##phoneDetailModal"
-    >
-      Show Details
-    </button>
-  </div>
-</div>
-
+              <img src="${phone.image}" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details </button>
+              </div>
+            </div>
     `;
     phonesContainer.appendChild(phoneDiv);
   });

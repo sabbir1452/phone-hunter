b902323225d16dc3c0fea4df8802e699ee8237f3
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-05 19:17:07 +0600

    lkdj;f

--- js/app.js
+++ js/app.js
@@ -39,7 +39,8 @@ const displayPhones = (phones, dataLimit) => {
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details </button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal"
+                data-bs-target="#phoneDetailModal">Show Details </button>
               </div>
             </div>
     `;
@@ -90,7 +91,29 @@ const loadPhoneDetails = async (id) => {
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.data);
+  displayPhoneDetails(data.data);
 };
 
-// loadPhones();
+const displayPhoneDetails = (phone) => {
+  console.log(phone);
+  const modalTitle = document.getElementById("phoneDetailModalLabel");
+  modalTitle.innerText = phone.name;
+  const phoneDetails = document.getElementById("phone-details");
+  phoneDetails.innerHTML = `
+  <p> Release Date:${
+    phone.releaseDate ? phone.releaseDate : "No Release Date Found"
+  }</p>
+
+  <p>Storage: ${
+    phone.mainFeatures
+      ? phone.mainFeatures.storage
+      : "No Storage Information Found"
+  }</p>
+
+  <p> Others: ${
+    phone.others ? phone.others.Bluetooth : "No Blutooth Information"
+  }
+  `;
+};
+
+loadPhones("apple");

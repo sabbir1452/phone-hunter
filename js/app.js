b1651b38f263fb572fe7992fb3e1d9a3ba81c456
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-05 18:14:47 +0600

    lkdj;f

--- js/app.js
+++ js/app.js
@@ -31,17 +31,24 @@ const displayPhones = (phones, dataLimit) => {
     phoneDiv.classList.add("col");
     phoneDiv.innerHTML = `
     <div class="card p-4">
-              <img src="${phone.image}" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-                <button href="#" class="btn btn-primary">Show Details </button>
-              </div>
-            </div>
+  <img src="${phone.image}" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">${phone.phone_name}</h5>
+    <p class="card-text">
+      This is a longer card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </p>
+    <button
+      onclick="loadPhoneDetails('${phone.slug}')"
+      href="#"
+      class="btn btn-primary" data-bs-toggle="modal"
+      data-bs-target="##phoneDetailModal"
+    >
+      Show Details
+    </button>
+  </div>
+</div>
+
     `;
     phonesContainer.appendChild(phoneDiv);
   });
@@ -62,6 +69,17 @@ document.getElementById("btn-search").addEventListener("click", function () {
   processSearch(10);
 });
 
+// search input field enter key handler
+document
+  .getElementById("search-field")
+  .addEventListener("keypress", function (e) {
+    console.log(e.key);
+
+    if (e.key === "Enter") {
+      processSearch(10);
+    }
+  });
+
 const toggleSpinner = (isLoading) => {
   const loaderSection = document.getElementById("loader");
   if (isLoading) {
@@ -74,5 +92,12 @@ const toggleSpinner = (isLoading) => {
 document.getElementById("btn-show-all").addEventListener("click", function () {
   processSearch();
 });
+
+const loadPhoneDetails = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
+};
 
 // loadPhones();
